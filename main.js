9f779a0cb63d9882b6a49619366945626db673ff
Ben Sarmiento <me@bensarmiento.com>
2015-09-29 10:04:09 +0800

    parse query on url

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 var http = require('http');
+var url = require('url');
+var bencode = require('bencode');
 
 const VERSION = '1.0.0'
 
 var server = http.createServer(function(req, res) {
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('X-Tracker-Version', 'yowmamasita/tracker ' + VERSION);
-  // X-Tracker-Version
+
+  var queryObject = url.parse(req.url, true).query;
+  console.log(queryObject);
+
   res.writeHead(200);
   res.end('Hello Http');
 });
